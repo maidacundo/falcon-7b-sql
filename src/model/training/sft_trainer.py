c7b3f6ce1bf517dafc5e173336141a138ed717ea
maidacundo <maidacundo@gmail.com>
2023-07-10 11:23:38 +0200

    initial commit

---
+++ src/model/training/sft_trainer.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+import argparse
+import logging
+import os
+from functools import partial
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+
+import datasets
+import torch
+
+from torch import nn
+from torch.utils.data import DataLoader, Subset
+from tqdm import tqdm
+from transformers import PreTrainedModel, Trainer, TrainingArguments
+from transformers.trainer_pt_utils import IterableDatasetShard
+from transformers.trainer_utils import seed_worker
+from transformers.training_args import OptimizerNames
+from transformers.utils import is_datasets_available
+
+
+class CrossEntropyLoss(nn.CrossEntropyLoss):
+    def __init__(self, weight=None, size_average=None, ignore_index=-100, reduce=None, reduction="mean"):
+        super().__init__(weight, size_average, ignore_index, reduce, "none")
+        self._reduction = reduction
+
+    def forward(self, input, target, mask=None):
+        input = input.view(-1, input.size(-1))
+        target = target.view(-1)
+
+        if mask is not None:
+            mask = mask.view(-1).bool()
+            input = input[mask]
+            target = target[mask]
+
+        size = target.numel()
+
+        loss = super().forward(input, target)
+
+        if self._reduction == "none":
+            return loss
+        return loss.sum() / (size + 1e-8)
+
+def compute_metrics(eval_pred, preprocess_fns, metrics):
+    out = {}
+    for metric, preprocess_fn in zip(metrics, preprocess_fns):
+        preds, labels = preprocess_fn(eval_pred)
+        out = dict(**out, **metric.compute(predictions=preds, references=labels))
+
+    return out
+
+
+def preprocess_logits_for_metrics(logits, labels):
+    pred_ids = torch.argmax(logits, dim=-1)
+    return pred_ids
+
+
+class SFTTrainer(Trainer):
+    def __init__(
+        self,
+        model: Union[PreTrainedModel, nn.Module] = None,
+        args: TrainingArguments = None,
+        sampler: torch.utils.data.sampler.Sampler = None,
+        poly_eps: float = 1.0,
+        train_collate_fn: Callable = None,
+        **kwargs,
+    ):
+        super().__init__(model, args, **kwargs)
+        self.train_collate_fn = train_collate_fn
+        # By default CrossEntropyLoss ignores padding_index -100, but just in case use our own loss_fct
+        self.loss_fct = CrossEntropyLoss()
+        self.sampler = sampler
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels_mask = inputs.pop("label_masks")
+        targets = inputs.pop("targets")
+
+        outputs = model(
+            input_ids=inputs["input_ids"],
+            attention_mask=inputs.get("attention_mask", None),
+            use_cache=False,
+        )
+
+        loss = self.loss_fct(outputs.get("logits"), targets, mask=labels_mask)
+
+        return (loss, outputs) if return_outputs else loss
+
+    def _compute_loss(self, model, inputs):
+        inputs = self._prepare_inputs(inputs)
+
+        labels_mask = inputs.pop("label_masks")
+        targets = inputs.pop("targets")
+
+        outputs = model(
+            input_ids=inputs["input_ids"],
+            attention_mask=inputs.get("attention_mask", None),
+            use_cache=False,
+        )
+
+        logits = outputs.get("logits")
+
+        loss = self.loss_fct(outputs.get("logits"), targets, mask=labels_mask)
+
+        return loss, logits, targets, labels_mask
+
+    def prediction_step(
+        self,
+        model: nn.Module,
+        inputs: Dict[str, Union[torch.Tensor, Any]],
+        prediction_loss_only: bool,
+        ignore_keys: Optional[List[str]] = None,
+    ) -> Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]:
+        with torch.no_grad():
+            loss, logits, labels, labels_mask = self._compute_loss(model, inputs)
+            labels[~labels_mask.bool()] = -100  # padding_index
+
+        loss = loss.mean().detach()
+
+        if self.args.prediction_loss_only:
+            return (loss, None, None)
+
+        return (loss, logits, labels)
+
+    def get_train_dataloader(self):
+        data_collator = self.train_collate_fn
+        train_dataset = self.train_dataset
+        dataloader = DataLoader(
+            train_dataset,
+            batch_size=self.args.per_device_train_batch_size,
+            sampler=None,
+            collate_fn=data_collator,
+        )
+        return dataloader
---
+++ src/utils/training_utils.py
@@ -0,0 +1,129 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, set_seed
+from typing import List, NamedTuple
+import numpy as np
+import random
+from peft import prepare_model_for_kbit_training
+from peft import LoraConfig, get_peft_model
+
+
+SQL_SPECIAL_TOKENS = {
+    'schema': '<|schema|>',
+    'query': '<|query|>',
+    'sql': '<|sql|>',
+    'endoftext': '<|endoftext|>',
+}
+
+def set_seed(seed: int):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    set_seed(42)
+
+def get_device():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    return device
+
+def get_model(model_id: str, bnb_config: BitsAndBytesConfig):
+    model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device_map={"":0}, trust_remote_code=True)
+    return model
+
+def get_tokenizer(model_id: str):
+    tokenizer = AutoTokenizer.from_pretrained(model_id, do_lower_case=True)
+    return tokenizer
+
+def add_special_tokens_to_tokenizer(tokenizer, SQL_SPECIAL_TOKENS):
+    # add special tokens to the tokenizer
+    tokenizer.add_special_tokens(
+        {
+            "pad_token": tokenizer.eos_token,
+            "sep_token": tokenizer.eos_token,
+        }
+    )
+
+    additional_special_tokens = (
+        []
+        if "additional_special_tokens" not in tokenizer.special_tokens_map
+        else tokenizer.special_tokens_map["additional_special_tokens"]
+    )
+
+    additional_special_tokens = list(set(additional_special_tokens + list(SQL_SPECIAL_TOKENS.values())))
+
+    num_special_tokens = tokenizer.add_special_tokens({"additional_special_tokens": additional_special_tokens})
+
+    return tokenizer, num_special_tokens
+
+def add_embeddings_to_model(model, tokenizer, SQL_SPECIAL_TOKENS):
+    # map new tokens
+    new_tokens_ids = {}
+
+    for value in SQL_SPECIAL_TOKENS.values():
+      ids = tokenizer(value)['input_ids']
+      if len(ids) > 1:
+        new_tokens_ids[value] = ids
+
+    # retrieve embedding weigths
+    embeddings_key = ''
+    for k in model.state_dict().keys():
+        if 'word_embeddings.weight' in k:
+            embeddings_key = k
+            break
+    embeddings_weights = model.state_dict()[embeddings_key].clone()
+
+    # calculate new tokens embeddings weigths as mean of the sub-words embeddings
+    new_tokens_emb_weights = {}
+
+    for token in new_tokens_ids.keys():
+      token_emb_list = []
+      for ids in new_tokens_ids[token]:
+        token_emb = embeddings_weights[ids].clone()
+        token_emb_list.append(token_emb)
+      token_emb_tensor = torch.stack(token_emb_list)
+      # mean of the sub-words embeddings
+      mean_emb = torch.mean(token_emb_tensor, dim=0)
+      new_tokens_emb_weights[token] = mean_emb
+
+    # add new tokens and special tokens to the tokenizer vocab
+    tokenizer, num_special_tokens = add_special_tokens_to_tokenizer(tokenizer, SQL_SPECIAL_TOKENS)
+
+    # resize model embedding
+    model.resize_token_embeddings(len(tokenizer))
+
+    vocab_size = tokenizer.vocab_size
+
+    new_embs = model.state_dict()[embeddings_key][
+        vocab_size : vocab_size + num_special_tokens, :
+    ].clone()
+
+    # add new weights to the model.state_dict
+    for token in new_tokens_emb_weights.keys():
+      new_ids = tokenizer(token)['input_ids']
+      model.state_dict()[embeddings_key][new_ids] = new_tokens_emb_weights[token]
+    
+    return new_embs
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+def get_model_and_tokenizer(model_id: str, bnb_config: BitsAndBytesConfig, lora_config: LoraConfig, SQL_SPECIAL_TOKENS: dict):
+    tokenizer = get_tokenizer(model_id)
+    model = get_model(model_id, bnb_config)
+    add_embeddings_to_model(model, tokenizer, SQL_SPECIAL_TOKENS)
+    model.gradient_checkpointing_enable()
+    model = prepare_model_for_kbit_training(model)
+    model = get_peft_model(model, lora_config)
+    print_trainable_parameters(model)
+    return model, tokenizer
